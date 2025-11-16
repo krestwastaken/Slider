f88157062a3ce84314729d8e76e547152d473a3a
krestwastaken <martynskelvin@gmail.com>
2025-11-16 21:13:14 +0100

    settings

--- src/ReactCarousel.jsx
+++ src/ReactCarousel.jsx
@@ -12,6 +12,9 @@ const ReactCarousel = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: true
   };
   return (
     <section className="slick-container">
